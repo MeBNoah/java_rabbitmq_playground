cc461799bd119b201778e667c4b640d31b2a7259
Noah Ispas <noahispas@gmail.com>
2016-06-22 19:03:41 +0200

    receivers use the same queue

--- src/main/java/com/exchange/direct/explicit/DirectMessageReceiver.java
+++ src/main/java/com/exchange/direct/explicit/DirectMessageReceiver.java
@@ -14,21 +14,23 @@ import java.util.concurrent.TimeoutException;
  */
 public class DirectMessageReceiver {
 
+    private final static String QUEUE_NAME = "executor_queue";
+
     public static void main(String[] argv) throws IOException, InterruptedException, TimeoutException {
 
         Connection connection = ConnectionHelper.connect();
         Channel channel = connection.createChannel();
 
         channel.exchangeDeclare(DirectMessageSender.EXCHANGE_NAME, "direct");
-        String queueName = channel.queueDeclare().getQueue();
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
 
         //hier kann man auch mehrere routing keys binden
-        channel.queueBind(queueName, DirectMessageSender.EXCHANGE_NAME, DirectMessageSender.ROUTING_KEY);
+        channel.queueBind(QUEUE_NAME, DirectMessageSender.EXCHANGE_NAME, DirectMessageSender.ROUTING_KEY);
 
         System.out.println(" [*] Waiting for messages");
 
         QueueingConsumer consumer = new QueueingConsumer(channel);
-        channel.basicConsume(queueName, true, consumer);
+        channel.basicConsume(QUEUE_NAME, true, consumer);
 
         while (true) {
             QueueingConsumer.Delivery delivery = consumer.nextDelivery();
